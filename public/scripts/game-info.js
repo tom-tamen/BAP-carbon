e7d74823e9f8b0b10ded4bc90d355f7524beba70
Janknn <jerem.pelchat@gmail.com>
2022-12-15 16:39:13 +0100

    :sparkles: local storage homepage

---
+++ public/scripts/game-info.js
@@ -0,0 +1,46 @@
+window.reload = localStorage.clear();
+
+let players = [];
+
+function test(){
+    if (players.length<8) {
+        let playerList = document.getElementById("player-list")
+        let a = document.getElementById('name').value
+        players.push(a)
+        while (playerList.firstChild) {
+        playerList.removeChild(playerList.firstChild);
+        localStorage.setItem('Players', players)
+        }
+        for (let i = 0; i < players.length; i++) {
+            let p = document.createElement("p")
+            p.innerHTML=players[i]
+            playerList.appendChild(p)
+        }
+    } else {
+        console.log("2 many players")
+    }
+}
+
+let lap = null;
+let frequency = null;
+
+document.querySelectorAll('.lap').forEach(e =>{
+    e.addEventListener( 'click', ()=>{
+        localStorage.setItem('nbTour', e.value)
+        console.log(localStorage);
+
+        let input = document.getElementById("client-value");
+        input.addEventListener("keyup", function(event) {
+            event.preventDefault();
+            document.getElementById("client-value").click();
+        });
+    })  
+})
+
+document.querySelectorAll('.frequency').forEach(e =>{
+    e.addEventListener( 'click', ()=>{
+        localStorage.setItem('frequency', e.value)
+        console.log(localStorage);
+    })
+})
+  
--- public/scripts/player-info.js
+++
@@ -1,2 +0,0 @@
-localStorage.clear();
-
