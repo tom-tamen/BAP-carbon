8885ee0bfe12fa2417554e8f738eb25c7068c5aa
Tom Tamen <tomtamen@gmx.fr>
2022-12-16 12:07:31 +0100

    jeu en cours ajout du js

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,9 +1,11 @@
-let nbTours = 5
+let nbTours = 10
 let frequency = 0.5
 let compteTours = 1
 
-let tourPrint = document.querySelector('.tour')
-tourPrint.innerHTML = "Tour "+compteTours+"<span>/"+nbTours+"</span>"
+let tourPrint = document.querySelectorAll('.tour')
+tourPrint.forEach(tour=>{
+    tour.innerHTML = "Tour "+compteTours+"<span>/"+nbTours+"</span>"
+})
 
 let eventName = document.querySelector('.eventName')
 let eventDesc = document.querySelector('.eventDesc')
@@ -28,22 +30,45 @@ function resetEvent(){
     eventDesc.innerText = "Vous pouvez continuer la partie"
 }
 
-document.querySelector('.next').addEventListener('click', ()=>{
-    if(nbTours === compteTours){
-        //do things in localstorage
-        window.location.href = "../select-cards.html";
-    }else{
-        compteTours++
-        tourPrint.innerHTML = "Tour "+compteTours+"<span>/"+nbTours+"</span>"
-
-        if(hasEvent(frequency)){
-            fetchEvent(Math.floor(Math.random() * 18) + 1).then(fetchedEvent => {
-                console.log(fetchedEvent)
-                eventName.innerText = fetchedEvent.name.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-                eventDesc.innerText = fetchedEvent.effect
-            })
+document.querySelectorAll('.next').forEach(btn=>{
+    btn.addEventListener('click', ()=>{
+        if(nbTours === compteTours){
+            //do things in localstorage
+            window.location.href = "../select-cards.html";
         }else{
-            resetEvent()
+            compteTours++
+            tourPrint.forEach(tour=>{
+                tour.innerHTML = "Tour "+compteTours+"<span>/"+nbTours+"</span>"
+            })
+    
+            if(hasEvent(frequency)){
+                fetchEvent(Math.floor(Math.random() * 18) + 1).then(fetchedEvent => {
+                    // console.log(fetchedEvent)
+                    eventName.innerText = fetchedEvent.name.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+                    eventDesc.innerText = fetchedEvent.effect
+                })
+            }else{
+                resetEvent()
+            }
         }
-    }
+    })
+})
+
+
+//----------------------------------------------------------------------
+//Changement de page :
+
+let GameTour = document.querySelector('.div-game-tour')
+let GameEvent = document.querySelector('.div-game-event')
+
+let btnEvent=document.querySelector('.btn-event')
+let btnTour=document.querySelector('.btn-tour')
+
+btnTour.addEventListener('click', ()=>{
+    GameTour.classList.remove('hide')
+    GameEvent.classList.add('hide')
+})
+btnEvent.addEventListener('click', ()=>{
+    GameTour.classList.add('hide')
+    GameEvent.classList.remove('hide')
 })
